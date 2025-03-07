643bb108c8650139fa20eee61ce860ffcd48548f
SRD4RW1N-code <daportillor@ufpso.edu.co>
2025-03-06 20:42:46 -0500

    projecto sprint boot
    aquí se hará el proyecto final de la clase

---
+++ DROGUERIA_PDN/src/test/java/drogueria_pdn/DROGUERIA_PDN/DrogueriaPdnApplicationTests.java
@@ -0,0 +1,13 @@
+package drogueria_pdn.DROGUERIA_PDN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrogueriaPdnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
